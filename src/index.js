5f5e7060076ae7e4817a3671e613292fc69f4e76
vhung0206 <vhung0206@gmail.com>
2021-06-29 09:37:25 +0700

    boostrap, routing, search

--- src/index.js
+++ src/index.js
@@ -11,16 +11,33 @@ app.use(morgan('combined'))
 app.set('views', path.join(__dirname, 'resources/views'));
 app.engine('hbs', handlebars({extname: '.hbs'}));
 app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 const port = 3000
 
 
 app.get('/', (req, res) => {
+  console.log(req.query.keyword);
+  res.render('home');
+})
+
+app.post('/', (req, res) => {
   res.render('home');
 })
 
 app.get('/new', (req, res) => {
   res.render('register');
+})
+
+
+app.get('/search', (req, res) => {
+  res.render('search');
+})
+
+app.post('/search', (req, res) => {
+  res.render('search');
 })
 
 app.listen(port, () => {
